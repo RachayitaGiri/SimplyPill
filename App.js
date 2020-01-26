5f3d1b451d5681756fd7e6f9fa296b814f44d519
Rachayita Giri <rachayitagiri@gmail.com>
2020-01-26 12:19:09 -0800

    initial setup

---
+++ App.js
@@ -0,0 +1,32 @@
+// import React from 'react';
+// import { StyleSheet, Text, View } from 'react-native';
+
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>Hello, world!</Text>
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
+
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+export default class HelloWorldApp extends Component {
+  render() {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center"}}>
+        <Text>Hello, world!</Text>
+      </View>
+    )
+  }
+}
